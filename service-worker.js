590ad91161ea667c4f4578d9253733c968700581
yutameta0201-dev <yutameta0201@gmail.com>
2026-06-02 17:49:28 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,43 @@
-const CACHE_NAME = "inspection-v1";
+const CACHE_NAME = "inspection-v2";
 
 const urlsToCache = [
   "./",
-  "./index.html"
+  "./index.html",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-
   );
 
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
 
   event.respondWith(
-
     caches.match(event.request)
       .then(response => {
-
         return response || fetch(event.request);
-
       })
-
   );
 
 });
